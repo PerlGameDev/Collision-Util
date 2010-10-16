22356ca81ff565935d1fd6625b0b3cbd9f264ddc
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-15 20:28:01 -0400

    Add Collision::Util::Rect;

---
+++ lib/Collision/Util/Rect.pm
@@ -0,0 +1,122 @@
+package Collision::Util::Rect;
+
+use strict;
+use warnings;
+
+use Collision::Util ':std';
+use Scalar::Util 'refaddr';
+
+my %_x;
+my %_y;
+my %_w;
+my %_h;
+my %_v_x;
+my %_v_y;
+
+sub new {
+    my ( $class, %options ) = @_;
+
+    my $self = bless do { \my $anon_scalar }, $class;
+
+    my $id = refaddr $self;
+    $_x{$id}   = $options{x};
+    $_y{$id}   = $options{y};
+    $_w{$id}   = $options{w};
+    $_h{$id}   = $options{h};
+    $_v_x{$id} = exists $options{v_x} ? $options{v_x} : 0;
+    $_v_y{$id} = exists $options{v_y} ? $options{v_y} : 0;
+
+    return $self;
+}
+
+sub DESTROY {
+    my ($self) = @_;
+
+    my $id = refaddr $self;
+    delete $_x{$id};
+    delete $_y{$id};
+    delete $_w{$id};
+    delete $_h{$id};
+    delete $_v_x{$id};
+    delete $_v_y{$id};
+}
+
+sub x {
+    my ( $self, $x ) = @_;
+    my $id = refaddr $self;
+    $_x{$id} = $x if defined $x;
+    return $_x{$id};
+}
+
+sub y {
+    my ( $self, $y ) = @_;
+    my $id = refaddr $self;
+    $_y{$id} = $y if defined $y;
+    return $_y{$id};
+}
+
+sub w {
+    my ( $self, $w ) = @_;
+    my $id = refaddr $self;
+    $_w{$id} = $w if defined $w;
+    return $_w{$id};
+}
+
+sub h {
+    my ( $self, $h ) = @_;
+    my $id = refaddr $self;
+    $_h{$id} = $h if defined $h;
+    return $_h{$id};
+}
+
+sub v_x {
+    my ( $self, $v_x ) = @_;
+    my $id = refaddr $self;
+    $_v_x{$id} = $v_x if defined $v_x;
+    return $_v_x{$id};
+}
+
+sub v_y {
+    my ( $self, $v_y ) = @_;
+    my $id = refaddr $self;
+    $_v_y{$id} = $v_y if defined $v_y;
+    return $_v_y{$id};
+}
+
+sub top {
+    my ($self) = @_;
+    my $id = refaddr $self;
+    return $_y{$id};
+}
+
+sub bottom {
+    my ($self) = @_;
+    my $id = refaddr $self;
+    return $_y{$id} + $_h{$id};
+}
+
+sub left {
+    my ($self) = @_;
+    my $id = refaddr $self;
+    return $_x{$id};
+}
+
+sub right {
+    my ($self) = @_;
+    my $id = refaddr $self;
+    return $_x{$id} + $_w{$id};
+}
+
+sub intersects {
+    my ( $self, $rect ) = @_;
+    return $self->check_collision($rect);
+}
+
+sub contains {
+    my ( $self, $rect ) = @_;
+    return $self->check_contains($rect);
+}
+
+1;
+
+__END__
